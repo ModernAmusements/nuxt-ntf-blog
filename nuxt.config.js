8fdfe5e45a4cebe5433081004ca2d40d1ad8875e
shady <shady@epunks.de>
2022-06-22 13:26:43 +0200

    changes in nuxt config for daisy ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,9 @@ import postcssPresetEnv from 'postcss-preset-env'
 import postcssEasingGradients from 'postcss-easing-gradients'
 import * as SITE_INFO from './content/site/info.json'
 import { COLOR_MODE_FALLBACK } from './utils/globals.js'
+import { defineNuxtConfig } from 'nuxt'
 
-export default {
+export default defineNuxtConfig({
   target: 'static',
   components: true,
   generate: {
@@ -143,4 +144,4 @@ export default {
       ogImage: 'static/mondern_amusement.png',
     },
   },
-}
+})
