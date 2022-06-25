d8ca72a928f93ec10a3a3df933a2ab49778e9ec2
shady <shady@epunks.de>
2022-06-25 12:12:53 +0200

    default layout import

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,13 @@
+<script>
+import Header from "@/components/global/Header.vue"
+import Footer from "@/components/global/Footer.vue"
+export default {
+  components: {
+    Header,
+    Footer
+  },
+};
+</script>
 <template>
   <div class="wrapper">
     <Header />
