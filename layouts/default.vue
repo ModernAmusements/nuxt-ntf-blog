262302d3bf64afb817a7243898da2ab6c58b2239
shady <shady@epunks.de>
2022-07-06 20:53:35 +0200

    meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,21 @@
+<script>
+export default {
+  head: {
+    title: 'my website title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'Modern Amusement Shady Tawfik',
+        name: 'Modern Amusement Shady Tawfik',
+        content: 'Shady Tawfik is a German JavaScript/PHP Developer, and a Quality/ Performance Engineer.Fluent in PHP and JavaScript.'
+      }
+    ],
+    link: [{ rel: 'og:image', href: '/static/preview-meta.jpg' }]
+  }
+}
+</script>
+  
 <template>
   <div class="wrapper">
     <Header />
