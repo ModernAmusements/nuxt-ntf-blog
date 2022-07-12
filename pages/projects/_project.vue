33dcc4fc76a3f36ffcaf2da306eb5d4ea84022f7
shady <shady@epunks.de>
2022-07-12 20:34:01 +0200

    code color dark and light

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -7,9 +7,11 @@
 
       <article>
         <img v-if="post.cover" class="cover-image" :src="post.cover">
-        <h6 class="inline py-1 px-2 mr-1 bg-gray text-white text-sm font-medium rounded-sm">{{ post.category }}</h6>
+        <h6 class="inline py-1 px-2 mr-1 bg-gray dark:text-white text-primary-400 text-sm font-medium rounded-sm">{{
+            post.category
+        }}</h6>
         <h1 class="">{{ post.title }}</h1>
-        <p class="mt-1 mb-8 text-primary-600 dark:text-primary-400">{{ post.description }}</p>
+        <p class="mt-1 mb-8  dark:text-primary-400">{{ post.description }}</p>
         <nuxt-content :document="post" />
         <div v-if="post.gallery" class="nuxt-content">
           <img v-for="image in post.gallery" class="image" :key="image.id" :src="image">
