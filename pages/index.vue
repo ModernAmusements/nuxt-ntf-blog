8060943e6d495873a18291d3e67a2627a22c05d2
shady <shady@epunks.de>
2022-07-16 18:13:20 +0200

    revert

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <script>
 import HeroImg from "@/assets/img/ma-hero.svg";
 import blogIcon from "@/assets/img/ma-homepage-form-section-2.svg";
-import MouseMove from "../components/global/MouseMove.vue";
 export default {
   head() {
     return {
@@ -26,7 +25,6 @@ export default {
       blogIcon,
     };
   },
-  components: { MouseMove },
 };
 </script>
 
@@ -96,8 +94,7 @@ export default {
           </form>
         </div>
         <!-- Homepage Img -->
-        <!-- <img class="mt-8 md:mt-0 w-100" :src="HeroImg" alt="..." /> -->
-        <MouseMove />
+        <img class="mt-8 md:mt-0 w-100" :src="HeroImg" alt="..." />
       </div>
     </section>
     <section class="mt-8">
