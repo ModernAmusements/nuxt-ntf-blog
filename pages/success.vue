7040f3d7da83d1d48cf67f152c6854a70ab19b30
shady <shady@epunks.de>
2022-07-24 14:32:02 +0200

    succes

--- pages/success.vue
+++ pages/success.vue
@@ -1,84 +1,84 @@
 <script>
-import patternVue from '@/assets/img/pattern_vue.png'
-import blogIcon from '@/assets/img/blog_icon.png'
+import patternVue from "@/assets/img/pattern_vue.png";
+import blogIcon from "@/assets/img/blog_icon.png";
 export default {
-	head() {
-		return {
-			title: "Success => ModernAmusement",
-			meta: [
-				{
-					name: 'og:description',
-					property: 'og:description',
-					content: 'Success => Shady Tawfik Bielefeld',
-				},
-				{
-					name: 'description',
-					property: 'description',
-					content: 'Success => Shady Tawfik Bielefeld',
-				},
-			]
-		};
-	},
-	data() {
-		return {
-			patternVue,
-			blogIcon
-		}
-	},
-}
+  head() {
+    return {
+      title: "Success => ModernAmusement",
+      meta: [
+        {
+          name: "og:description",
+          property: "og:description",
+          content: "Success => Shady Tawfik Bielefeld",
+        },
+        {
+          name: "description",
+          property: "description",
+          content: "Success => Shady Tawfik Bielefeld",
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      patternVue,
+      blogIcon,
+    };
+  },
+};
 </script>
 
 <template>
-	<main>
-		<section>
-			<div class="content-wrapper-col">
-				<div class="newsletter-success text-center">
-					<h2 class="underline underline-offset-1 text-lg">Thank you for your registration</h2>
-					<h1 class="display">
-						<span class="text-primary">Welcome to my Webdev Blog</span>
-					</h1>
-					<p class="text-md">
-						Don't worry i send out newsletters every 2 weeks and you can unsubscribe at any time
-					</p>
-					<nuxt-link to="/"
-						class="text-sm btn pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent">
-						Back Home ==>
-					</nuxt-link>
-				</div>
-				<img class="mt-8 md:mt-0 w-100" :src="patternVue" alt="..." />
-			</div>
-		</section>
-	</main>
+  <main>
+    <section>
+      <div class="content-wrapper-col">
+        <div class="newsletter-success text-center">
+          <h2 class="underline underline-offset-1 text-lg">Thank you for your Request</h2>
+          <h1 class="display">
+            <span class="text-primary">Welcome to my World</span>
+          </h1>
+          <p class="text-md">I respond 1-2 business days.</p>
+          <nuxt-link
+            to="/"
+            class="text-sm btn pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent"
+          >
+            Back Home ==>
+          </nuxt-link>
+        </div>
+        <img class="mt-8 md:mt-0 w-100" :src="patternVue" alt="..." />
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
 .content-wrapper-col {
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-	justify-content: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
 }
 
 img {
-	width: 50%;
-	height: auto;
-	object-fit: cover;
+  width: 50%;
+  height: auto;
+  object-fit: cover;
 }
 
 input {
-	height: 2.6rem;
+  height: 2.6rem;
 }
 
 @media (max-width: 780px) {
-	.content-wrapper {
-		flex-direction: column;
-		justify-content: space-between;
-	}
+  .content-wrapper {
+    flex-direction: column;
+    justify-content: space-between;
+  }
 
-	img {
-		width: 100%;
-		height: auto;
-		object-fit: cover;
-	}
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
 }
 </style>
